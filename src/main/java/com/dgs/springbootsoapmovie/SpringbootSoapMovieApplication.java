98fee838f52412c83b92fd0734d22739aaad63a5
Dragos Sarpe <contact@usdgadget.com>
2018-09-28 14:54:30 +0300

    Initial import of Java project

---
+++ src/main/java/com/dgs/springbootsoapmovie/SpringbootSoapMovieApplication.java
@@ -0,0 +1,12 @@
+package com.dgs.springbootsoapmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSoapMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSoapMovieApplication.class, args);
+	}
+}
