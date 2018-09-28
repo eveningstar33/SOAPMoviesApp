a1a936c28e62258c55fee7111ceafe02ae61ede6
Dragos Sarpe <contact@usdgadget.com>
2018-09-28 17:26:13 +0300

    Added Service package

---
+++ src/main/java/com/dgs/springbootsoapmovie/soap/service/MovieDetailsService.java
@@ -0,0 +1,13 @@
+package com.dgs.springbootsoapmovie.soap.service;
+
+import com.dgs.springbootsoapmovie.soap.bean.Movie;
+
+public class MovieDetailsService implements MovieService {
+
+	@Override
+	public Movie getMovieById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/com/dgs/springbootsoapmovie/soap/service/MovieService.java
@@ -0,0 +1,8 @@
+package com.dgs.springbootsoapmovie.soap.service;
+
+import com.dgs.springbootsoapmovie.soap.bean.Movie;
+
+public interface MovieService {
+
+	public Movie getMovieById(int id);
+}
