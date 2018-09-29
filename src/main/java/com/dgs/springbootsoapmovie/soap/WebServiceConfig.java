065245769faa8b9c7a88c6f450020751caab727f
Dragos Sarpe <contact@usdgadget.com>
2018-09-29 13:59:51 +0300

    Added Security implementation with WS-Security

--- src/main/java/com/dgs/springbootsoapmovie/soap/WebServiceConfig.java
+++ src/main/java/com/dgs/springbootsoapmovie/soap/WebServiceConfig.java
@@ -1,5 +1,8 @@
 package com.dgs.springbootsoapmovie.soap;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -67,5 +73,42 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 	public XsdSchema coursesSchema() {
 		
 		return new SimpleXsdSchema(new ClassPathResource("movie-details.xsd"));
+	}
+	
+	// Security configuration
+	
+	// XwsSecurityInterceptor
+
+	// Interceptors.add -> XwsSecurityInterceptor
+	
+	@Bean
+	public XwsSecurityInterceptor securityInterceptor() {
+		
+		XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+		
+		// We have a securityInterceptor but we would need to add it to be processed by the list of Spring Web Services interceptors
+		// How do we do that? We need to extend a specific class WsConfigurerAdapter
+		
+		// Callback Handler -> SimplePasswordValidationCallbackHandler
+		// Security Policy -> securityPolicy.xml
+		
+		securityInterceptor.setCallbackHandler(callbackHandler()); 
+		securityInterceptor.setPolicyConfiguration(new ClassPathResource("securityPolicy.xml"));
+		
+		return securityInterceptor;
+	}
+	
+	public SimplePasswordValidationCallbackHandler callbackHandler() {
+
+		SimplePasswordValidationCallbackHandler handler = new SimplePasswordValidationCallbackHandler();
+		
+		handler.setUsersMap(Collections.singletonMap("user", "password")); 
+		
+		return handler; 
+	}
+	
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {
+		interceptors.add(securityInterceptor());
 	}
 }
